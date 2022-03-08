4e337d86f75f63edcc627bfbc2a5a7ad6927f864
bscott4 <brodyscott12+dev@gmail.com>
2022-03-07 21:47:42 -0800

    webhookHandler

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,11 @@
 import * as functions from "firebase-functions";
-import { Client, resources } from "coinbase-commerce-node";
-import type { CreateCharge } from "coinbase-commerce-node";
-import { cors } from "./cors";
+import {Client, Webhook, resources} from "coinbase-commerce-node";
+import type {CreateCharge} from "coinbase-commerce-node";
+import {cors} from "./cors";
 
 Client.init("ea5f684e-7558-420b-b2b6-4c091f32d8da");
 
-const { Charge } = resources;
+const {Charge} = resources;
 
 export const createCharge = functions.https.onRequest((req, res) => {
   cors(req, res, async () => {
@@ -27,4 +27,35 @@ export const createCharge = functions.https.onRequest((req, res) => {
 
     res.send(charge);
   });
+});
+
+export const webhookHandler = functions.https.onRequest(async (req, res) => {
+  const rawBody = req.rawBody;
+  const signature = req.headers["x-cc-webhook-signature"];
+  const webhookSecret = "your-webhook-secret";
+
+  try {
+    const event = Webhook.verifyEventBody(
+        rawBody.toString(),
+      signature as string,
+      webhookSecret
+    );
+
+    if (event.type === "charge:pending") {
+      functions.logger.log("pending");
+    }
+
+    if (event.type === "charge:confirmed") {
+      functions.logger.log("confirmed");
+    }
+
+    if (event.type === "charge:failed") {
+      functions.logger.log("no bueno");
+    }
+
+    res.send(`success ${event.id}`);
+  } catch (error) {
+    functions.logger.error(error);
+    res.status(400).send("failure!");
+  }
 });
